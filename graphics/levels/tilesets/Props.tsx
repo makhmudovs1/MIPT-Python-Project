642be39a32ccbe790818d016b486c99dec59699d
Sheravgan <makhmudov.shb@gmail.com>
2023-04-20 05:33:54 +0300

    add graphics

---
+++ graphics/levels/tilesets/Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Props" tilewidth="64" tileheight="64" tilecount="50" columns="5">
+ <image source="../../decoration/props.png" width="383" height="640"/>
+</tileset>
---
+++ graphics/levels/tilesets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="DarkCastle (1) (1)" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="../../../../../Downloads/DarkCastle (1) (1).png" width="320" height="320"/>
+</tileset>
